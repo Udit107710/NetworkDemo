1848d4953856ea29ecdc7a583ed84c45b84d6b48
Udit107710 <uditcr710107@gmail.com>
2020-02-09 17:32:46 +0530

    updated

--- script.py
+++ script.py
@@ -2,17 +2,24 @@ from pcapng import FileScanner
 from scapy.all  import *
 from collections import defaultdict
 import networkx as nx
+import json 
 
-packets = rdpcap('data.pcap')
-d = defaultdict(list)
-graph = nx.Graph()
-
-for packet in packets:
-    if IP in packet:
-#        print(packet[IP].src, packet[IP].dst)
-        graph.add_node(packet[IP].src)
-        graph.add_node(packet[IP].dst)
-        graph.add_edge(packet[IP].src, packet[IP].dst)
+with open('data.json') as f:
+    packets = json.load(f)
+    graph = nx.Graph()
+    color_map = []
+    for packet in packets:
+        if "tcp" in packet["_source"]["layers"].keys():
+            if packet["_source"]["layers"]["tcp"]["tcp.seq"] == "1" and packet["_source"]["layers"]["tcp"]["tcp.ack"] == "1" and packet["_source"]["layers"]["tcp"]["tcp.len"] == "0":
+                
+                try:
+                    graph.add_node(packet["_source"]["layers"]["ip"]["ip.src"])
+                    graph.add_node(packet["_source"]["layers"]["ip"]["ip.dst"])
+                    graph.add_edge(packet["_source"]["layers"]["ip"]["ip.dst"], packet["_source"]["layers"]["ip"]["ip.src"])
+                except KeyError:
+                    graph.add_node(packet["_source"]["layers"]["ipv6"]["ipv6.src"])
+                    graph.add_node(packet["_source"]["layers"]["ipv6"]["ipv6.dst"])
+                    graph.add_edge(packet["_source"]["layers"]["ipv6"]["ipv6.dst"], packet["_source"]["layers"]["ipv6"]["ipv6.src"])
 
 nx.draw(graph, with_labels=True)
 plt.savefig("sample.png")
